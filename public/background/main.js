67da506be0b0e3491baa1105452c63ca485cd015
scs-tlei <scs-tlei@mx2.toyotasystems.com>
2020-02-18 15:41:00 +0900

    add class code

--- public/background/main.js
+++ public/background/main.js
@@ -59,7 +59,7 @@ function parseYoudao(collection) {
     var pronounces = [];
     collection.querySelectorAll(".pronounce").forEach((ele) => {
       pronounces.push({
-        language: ele.firstChild != null ? 
+        language: ele.firstChild != null ?
           ele.firstChild.textContent.replace(/^\s+|\s+$/g, "") : "",
         pronounce: ele.querySelector(".phonetic") != null ?
           ele.querySelector(".phonetic").textContent.replace(/^\s+|\s+$/g, "") : ""
@@ -86,18 +86,17 @@ function parseYoudao(collection) {
         translations: []
       }
     }
-  } catch(err) {
+  } catch (err) {
     return {
       translations: []
     }
   }
 }
 
-class Youdao {
+class Dict {
   constructor(word, sendResponse) {
     this.word = this.normalize(word);
     this.sendResponse = sendResponse;
-    this.translateWord();
   }
 
   normalize = (word) => {
@@ -106,6 +105,74 @@ class Youdao {
     return word;
   }
 
+  getFromCache = () => {
+    return null;
+  }
+
+  translateWord = () => {
+    var xhr = new XMLHttpRequest();
+    var url = this.urlBase + this.word;
+    xhr.open('GET', url);
+    xhr.onload = () => {
+      if (xhr.status == 200) {
+        var parser = new DOMParser();
+        try {
+          var doc = parser.parseFromString(xhr.response, "text/html");
+          this.sendResponse(this.parseDoc(doc));
+        } catch (err) {
+          console.log(err);
+          this.sendNotFound();
+        }
+      }
+    };
+    xhr.onerror = () => {
+      this.sendError();
+    };
+    xhr.send();
+  }
+
+  sendNotFound = () => {
+    this.sendResponse({
+      pronounces: [],
+      translations: [{
+        translation: `parse error`
+      }]
+    });
+  }
+
+  sendError = () => {
+    this.sendResponse({
+      pronounces: [],
+      translations: [{
+        translation: `network error ..?`
+      }]
+    });
+  }
+}
+
+class Youdao extends Dict {
+  constructor(word, sendResponse) {
+    super(word, sendResponse);
+    this.urlBase = "https://youdao.com/w/";
+
+    let result = this.getFromCache();
+    if (result != null) {
+      this.sendResponse(result);
+    } else {
+      this.translateWord();
+    }
+  }
+
+  parseDoc = (doc) => {
+    var response = doc.getElementById("phrsListTab");
+    // TODO: if no #phrsListTab, fallback to networklist ?
+    
+    Array.from(response.children).forEach(ele => {
+      if (ele.localName === "script" || ele.localName === "style") {
+        response.removeChild(ele);
+      }
+    });
+  }
 
 }
 
